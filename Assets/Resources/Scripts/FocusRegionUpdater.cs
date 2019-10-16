73ec1f734ea1b348f02e88661271c85a31a197aa
Andreas Keller <andi.keller@tum.de>
2019-10-16 17:42:57 +0200

    Fix focus region not working

--- Assets/Resources/Scripts/FocusRegionUpdater.cs
+++ Assets/Resources/Scripts/FocusRegionUpdater.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
 
 public class FocusRegionUpdater : MonoBehaviour
 {
-    public GameObject Cursor;
+    public GazeProvider gazeProvider;
     public float focusSize = 0.15f;
 
     void Update()
     {
-        if (Cursor)
+        if (gazeProvider)
         {
-            Vector3 focusPosition = Cursor.transform.position;
+            Vector3 focusPosition = gazeProvider.HitPosition;
             GetComponent<Renderer>().material.SetVector("_FocusPosition", focusPosition);
             GetComponent<Renderer>().material.SetFloat("_FocusRadius", focusSize);
         }
