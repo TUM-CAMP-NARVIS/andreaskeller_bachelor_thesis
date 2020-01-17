08f922ecb85dc15ed4c10b2f0f467e3019f91831
Andreas Keller <andi.keller@tum.de>
2020-01-17 16:16:13 +0100

    Start implementing network-based vivetracking

---
+++ Assets/Scripts/TrackingMessage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TrackingMessage : MessageBase
+{
+    public uint netId;
+    public NetworkHash128 assetID;
+    public Vector3 position;
+    public Quaternion rotation;
+    
+
+    public override void Deserialize(NetworkReader reader)
+    {
+        netId = reader.ReadPackedUInt32();
+        assetID = reader.ReadNetworkHash128();
+        position = reader.ReadVector3();
+        rotation = reader.ReadQuaternion();
+    }
+
+    public override void Serialize(NetworkWriter writer)
+    {
+        writer.WritePackedUInt32(netId);
+        writer.Write(assetID);
+        writer.Write(position);
+        writer.Write(rotation);
+    }
+}
